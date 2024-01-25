7440f0e475b9124004bc5f5157fa9916cf84b776
Syluan <syluan82@gmail.com>
2024-01-25 09:07:49 +0700

    a

--- src/fa/training/services/Constants.java
+++
@@ -1,6 +0,0 @@
-package fa.training.services;
-
-public class Constants {
-	public static final String SHOW_ALL = "select * from card";
-	public static final String SAPXEP_ALL = "SELECT *FROM card ORDER BY type ASC , soDu DESC";
-}
